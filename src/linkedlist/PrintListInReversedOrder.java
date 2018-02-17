eaf3ebcbe2da2f96da15ff04fcb5165e463d24d0
chenlihuang <chenlihaung1994@hotmail.com>
2018-02-17 14:41:07 +0800

    从尾到头打印链表

---
+++ src/linkedlist/PrintListInReversedOrder.java
@@ -0,0 +1,48 @@
+package linkedlist;
+
+import java.util.Stack;
+
+import structure.ListNode;
+
+/**
+ * 从尾到头打印链表.
+ */
+public class PrintListInReversedOrder {
+
+  /**
+   * 从头便利链表，压入栈，弹出.
+   * @param node 链表表头
+   */
+  private static void printReversinglyIteratively(ListNode<Integer> node) {
+    Stack<Integer> stack = new Stack<>();
+    for (ListNode<Integer> temp = node; temp != null; temp = temp.next) {
+      stack.add(temp.val);
+    }
+    while (!stack.isEmpty()) {
+      System.out.println(stack.pop());
+    }
+  }
+
+  /**
+   * 递归，从表尾输出.
+   * @param node 链表表头
+   */
+  private static void printReversinglyRecursively(ListNode<Integer> node) {
+    if (node == null) {
+      return;
+    } else {
+      printReversinglyRecursively(node.next);
+      System.out.println(node.val);
+    }
+  }
+
+  public static void main(String[] args) {
+    ListNode<Integer> head = new ListNode<Integer>(1);
+    head.next = new ListNode<Integer>(2);
+    head.next.next = new ListNode<Integer>(3);
+    printReversinglyRecursively(head);
+    System.out.println();
+    printReversinglyIteratively(head);
+  }
+
+}
