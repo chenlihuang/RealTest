e329db18991ee496869caa36c4035c0d6f27ac97
chenlihuang <chenlihaung1994@hotmail.com>
2018-03-05 17:36:47 +0800

    层序遍历二叉树

--- src/structure/TreeNode.java
+++ src/structure/TreeNode.java
@@ -1,5 +1,8 @@
 package structure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode<T> {
   public T val;
   public TreeNode<T> left;
@@ -9,5 +12,28 @@ public class TreeNode<T> {
     this.val = val;
     this.left = null;
     this.right = null;
+  }
+
+  //层序遍历
+  @Override
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder("[");
+    Queue<TreeNode<T>> queue = new LinkedList<>();
+    queue.offer(this);
+    TreeNode<T> temp;
+    while (!queue.isEmpty()) {
+      temp = queue.poll();
+      stringBuilder.append(temp.val);
+      stringBuilder.append(",");
+      if (temp.left != null) {
+        queue.offer(temp.left);
+      }
+      if (temp.right != null) {
+        queue.offer(temp.right);
+      }
+    }
+    stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(","));
+    stringBuilder.append("]");
+    return stringBuilder.toString();
   }
 }
