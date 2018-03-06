d2243ec562abcbf16e92ca038e4c41cb066e0147
chenlihuang <chenlihaung1994@hotmail.com>
2018-03-06 17:32:08 +0800

    复杂链表结构 多一个random指针指向随机节点

---
+++ src/structure/ComplexListNode.java
@@ -0,0 +1,28 @@
+package structure;
+
+public class ComplexListNode {
+  public int val;
+  public ComplexListNode next;
+  public ComplexListNode random;
+
+  public ComplexListNode(int val) {
+    this.val = val;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder ret = new StringBuilder();
+    ComplexListNode cur = this;
+    while (cur != null) {
+      ret.append(cur.val);
+      if (cur.random != null) {
+        ret.append("(" + cur.random.val + ")");
+      } else {
+        ret.append("(_)");
+      }
+      ret.append('\t');
+      cur = cur.next;
+    }
+    return ret.toString();
+  }
+}
