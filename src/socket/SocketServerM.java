9ef6270df7543e4705f8faf9706d9d8d56b53f85
chenlihuang <chenlihaung1994@hotmail.com>
2018-03-21 14:43:44 +0800

    Socket相关 包括单线程server 多线程server 以及client

---
+++ src/socket/SocketServerM.java
@@ -0,0 +1,36 @@
+package socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SocketServerM {
+  public static void main(String[] args) throws IOException {
+
+    int port = 7000;
+    int clientNo = 1;
+
+    ServerSocket serverSocket = new ServerSocket(port);
+
+    // 创建线程池
+    ExecutorService exec = Executors.newCachedThreadPool();
+
+    try {
+
+      while (true) {
+        Socket socket = serverSocket.accept();
+//        exec.execute(new SingleServer(socket, clientNo));
+        exec.submit(new SingleServer(socket, clientNo));
+        clientNo++;
+        System.out.println(clientNo);
+      }
+
+    } finally {
+      serverSocket.close();
+    }
+
+  }
+
+}
